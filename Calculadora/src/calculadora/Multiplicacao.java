6184e038682af309d6ab20153ff26d348065ce09
BiancaWeiber <weiberbianca2@gmail.com>
2025-07-23 16:36:12 -0300

    feat: adiciona funcionalidade subtração

---
+++ Calculadora/src/calculadora/Multiplicacao.java
@@ -0,0 +1,20 @@
+package calculadora;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author info2
+ */
+public class Multiplicacao {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("digite o primeiro número");
+        int n1 = teclado.nextInt();
+        
+        System.out.println("digite o segundo número");
+        int n2 = teclado.nextInt();
+        
+        System.out.println("a multiplicacao dos números é : " + (n1*n2));
+    }
+}
