fd4d83c55c6098d2b4a1539d6cb66d6c5ba97913
BiancaWeiber <weiberbianca2@gmail.com>
2025-07-23 16:39:01 -0300

    feat: adiciona o funcionamento multiplicação

--- Calculadora/src/calculadora/Subtracao.java
+++ Calculadora/src/calculadora/Subtracao.java
@@ -1,20 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package calculadora;
-
-import java.util.Scanner;
 
 /**
  *
  * @author info2
  */
 public class Subtracao {
-    public static void main(String[] args) {
-        Scanner teclado = new Scanner(System.in);
-        System.out.println("digite o primeiro número");
-        int n1 = teclado.nextInt();
-        
-        System.out.println("digite o segundo número");
-        int n2 = teclado.nextInt();
-        
-        System.out.println("a subtracao dos números é : " + (n1-n2));
-    }
+    
 }
